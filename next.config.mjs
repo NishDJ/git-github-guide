487f5d308395e3951df1c86e5feb569f30a47e95
Nishant Jain <nishantjain1398@gmail.com>
2025-03-07 01:09:09 -0600

    removed github pages integration

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,10 @@
+let userConfig = undefined
+try {
+  userConfig = await import('./v0-user-next.config')
+} catch (e) {
+  // ignore error
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -6,8 +13,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Required for static export
-  output: 'export',
   images: {
     unoptimized: true,
   },
@@ -16,35 +21,28 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-  // Fixed basePath and assetPrefix for GitHub Pages
-  basePath: '/git-github-guide',
-  assetPrefix: '/git-github-guide/',
-  distDir: 'dist',
 }
 
-// Try to import user config
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-  
-  // Merge user config with next config
-  if (userConfig) {
-    for (const key in userConfig) {
-      if (
-        typeof nextConfig[key] === 'object' &&
-        !Array.isArray(nextConfig[key])
-      ) {
-        nextConfig[key] = {
-          ...nextConfig[key],
-          ...userConfig[key],
-        }
-      } else {
-        nextConfig[key] = userConfig[key]
+mergeConfig(nextConfig, userConfig)
+
+function mergeConfig(nextConfig, userConfig) {
+  if (!userConfig) {
+    return
+  }
+
+  for (const key in userConfig) {
+    if (
+      typeof nextConfig[key] === 'object' &&
+      !Array.isArray(nextConfig[key])
+    ) {
+      nextConfig[key] = {
+        ...nextConfig[key],
+        ...userConfig[key],
       }
+    } else {
+      nextConfig[key] = userConfig[key]
     }
   }
-} catch (e) {
-  // ignore error
 }
 
 export default nextConfig
